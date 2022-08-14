394d888561778a337fdb9c2c24ad916542d29597
Rune Monzel <runemonzel@googlemail.com>
2022-08-14 14:18:59 +0200

    updated version string to v0.2.0

--- ndsharray/__init__.py
+++ ndsharray/__init__.py
@@ -5,7 +5,7 @@ from ndsharray.ndsharray import supported_types
 
 __author__ = 'Rune Monzel'
 __email__ = 'runemonzel@googlemail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __all__ = ["ndsharray",
            "supported_types"]
 
