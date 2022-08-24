9d1a7f731897da004de763288194e1483e907f07
Rune Monzel <runemonzel@googlemail.com>
2022-08-24 15:15:28 +0200

    updated to version 1.0.1: inlcuding more documentation for pypi, readthedocs

--- ndsharray/__init__.py
+++ ndsharray/__init__.py
@@ -5,7 +5,7 @@ from ndsharray.ndsharray import supported_types
 
 __author__ = 'Rune Monzel'
 __email__ = 'runemonzel@googlemail.com'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __all__ = ["NdShArray",
            "supported_types"]
 
